e0989a968dda7e37730894170103e1cbce84b59a
sajjadul404 <sajjadul625@gmail.com>
2025-08-09 01:06:23 +0600

    A_Elephant

---
+++ 800/A_Anton_and_Letters.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s); 
+
+    set<char> letters; 
+
+    for (char c : s) {
+        if (c >= 'a' && c <= 'z') { 
+            letters.insert(c);
+        }
+    }
+
+    cout << letters.size() << endl;
+    return 0;
+}
