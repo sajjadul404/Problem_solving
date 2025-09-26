f94f97568f0685d64ea0490b03cac2d0dd06a6c8
sajjadul404 <sajjadul625@gmail.com>
2025-09-27 00:16:14 +0600

    A Homework

---
+++ 800/A_Homework.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using ull= unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<long long,long long>;
+
+#define endl '\n'
+#define prec(x)     fixed << setprecision(x)
+
+
+const ll INF  = (1LL<<60);
+const int MOD = 1'000'000'007;
+
+void fastio()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+
+void solve()
+{
+    int n;
+    cin>>n;
+
+    string s1;
+    cin>>s1;
+
+    int m;
+    cin>>m;
+
+    string s2,c;
+    cin>>s2>>c;
+
+    string str1="";
+    string str2="";
+    for(int i=0;i<m;i++)
+    {
+        if(c[i]=='V')
+        {
+            str1+=s2[i];
+        }
+        else if(c[i]=='D')
+        {
+            str2+=s2[i];
+        }
+    }
+
+    reverse(str1.begin(),str1.end());
+    cout<<str1+s1+str2<<endl;
+
+}
+
+int main()
+{
+    fastio();
+    //int T = 1;
+
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+}
