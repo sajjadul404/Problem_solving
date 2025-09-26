9854a1c23b2b541e4cd7ddc9d79406aff4867790
sajjadul404 <sajjadul625@gmail.com>
2025-09-27 00:17:57 +0600

    A HOmework

--- 800/A_Homework.cpp
+++ 800/A_Homework.cpp
@@ -1,19 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-using ld = long double;
-using ull= unsigned long long;
-using pii = pair<int,int>;
-using pll = pair<long long,long long>;
-
-#define endl '\n'
-#define prec(x)     fixed << setprecision(x)
-
-
-const ll INF  = (1LL<<60);
-const int MOD = 1'000'000'007;
-
 void fastio()
 {
     ios::sync_with_stdio(false);
