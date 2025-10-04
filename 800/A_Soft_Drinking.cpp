ec283b668f006de95eb7017264e5f4b0e06bf707
sajjadul404 <sajjadul625@gmail.com>
2025-10-04 22:34:05 +0600

    soft drink

--- 800/A_Soft_Drinking.cpp
+++ 800/A_Soft_Drinking.cpp
@@ -22,26 +22,3 @@ int main() {
 
     return 0;
 }
-
-
-
-
-
-
-
-
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-    int n, k, l, c, d, p, nl, np;
-    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
-
-    int total_drink = k * l;
-    int total_lime = c * d;
-    int drink_toast = total_drink / nl;   
-    int lime_toast = total_lime;          
-    int salt_toast = p / np;     
-    int total_toast = min({drink_toast, lime_toast, salt_toast});
-    cout << total_toast / n << endl;
-    return 0;
-}
