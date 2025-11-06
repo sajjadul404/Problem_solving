0ccf4c9ff51dc0f18728b2888441b12474e81a31
sajjadul404 <sajjadul625@gmail.com>
2025-11-06 20:53:58 +0600

    A Sequence Game

--- 800/A_Sequence_Game.cpp
+++ 800/A_Sequence_Game.cpp
@@ -8,17 +8,19 @@ int main() {
         int n;
         cin >> n;
         vector<long long> a(n);
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
-        }
+        for (int i = 0; i < n; i++) cin >> a[i];
         long long x;
         cin >> x;
 
         bool ok = false;
-        for (int i = 0; i < n - 1; i++) {
-            if (min(a[i], a[i + 1]) <= x && x <= max(a[i], a[i + 1])) {
-                ok = true;
-                break;
+        if (n == 1) {
+            ok = (a[0] == x);
+        } else {
+            for (int i = 0; i < n - 1; i++) {
+                if (min(a[i], a[i + 1]) <= x && x <= max(a[i], a[i + 1])) {
+                    ok = true;
+                    break;
+                }
             }
         }
 
