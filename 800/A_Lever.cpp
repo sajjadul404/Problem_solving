b6d6d5221b7fc93f4a73d4154caa51859b140c70
sajjadul404 <sajjadul625@gmail.com>
2025-11-07 23:54:54 +0600

    A Level 3

--- 800/A_Lever.cpp
+++ 800/A_Lever.cpp
@@ -1,24 +1,21 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <cstdio>
 
 int main() {
     int t;
-    cin >> t;
+    if (scanf("%d", &t) != 1) return 0;
     while (t--) {
         int n;
-        cin >> n;
+        scanf("%d", &n);
         int a[15], b[15];
-        for (int i = 0; i < n; i++) scanf("%d", &a[i]);
-        for (int i = 0; i < n; i++) scanf("%d", &b[i]);
+        for (int i = 0; i < n; ++i) scanf("%d", &a[i]);
+        for (int i = 0; i < n; ++i) scanf("%d", &b[i]);
         
-        int dec = 0, inc = 0;
-        for (int i = 0; i < n; i++) {
+        int dec = 0;
+        for (int i = 0; i < n; ++i) {
             if (a[i] > b[i]) dec += (a[i] - b[i]);
-            else inc += (b[i] - a[i]);
         }
         
-        int result = std::max(dec, inc) + 1;
-        cout << result << "\n";
+        printf("%d\n", dec + 1);
     }
     return 0;
 }
