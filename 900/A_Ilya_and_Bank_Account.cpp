0a6ca16d470c58fc5e002337cddac472da4b2c0d
sajjadul404 <sajjadul625@gmail.com>
2025-11-09 02:05:56 +0600

    A_Ilya_and_Bank_Account

---
+++ 900/A_Ilya_and_Bank_Account.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    if (n >= 0) {
+        cout << n;
+    } else {
+     
+        long long remove_last = n / 10;
+   
+        long long remove_second_last = (n / 100) * 10 + (n % 10);
+        cout << max(remove_last, remove_second_last);
+    }
+
+    return 0;
+}
