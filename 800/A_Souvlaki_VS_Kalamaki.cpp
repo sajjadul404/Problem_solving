a80a6ea7fada3b781bed77db163849fd9834157d
sajjadul404 <sajjadul625@gmail.com>
2025-11-10 21:47:57 +0600

    A Souvlaki VS Kalamaki

--- 800/A_Souvlaki_VS_Kalamaki.cpp
+++ 800/A_Souvlaki_VS_Kalamaki.cpp
@@ -1,9 +1,12 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
     int t;
     cin >> t;
+    
     while (t--) {
         int n;
         cin >> n;
@@ -11,19 +14,34 @@ int main() {
         for (int i = 0; i < n; i++) {
             cin >> a[i];
         }
+        bool hasDuplicate = false;
+        vector<int> b = a;
+        sort(b.begin(), b.end());
         
-        if (n % 2 == 0) {
+        for (int i = 1; i < n; i++) {
+            if (b[i] == b[i-1]) {
+                hasDuplicate = true;
+                break;
+            }
+        }
+        
+        if (hasDuplicate) {
             cout << "YES\n";
         } else {
-            bool duplicate = false;
+            bool sorted = true;
             for (int i = 1; i < n; i++) {
-                if (a[i] <= a[i-1]) {
-                    duplicate = true;
+                if (a[i] < a[i-1]) {
+                    sorted = false;
                     break;
                 }
             }
-            cout << (duplicate ? "YES\n" : "NO\n");
+            if (sorted) {
+                cout << "YES\n";
+            } else {
+                cout << "NO\n";
+            }
         }
     }
+    
     return 0;
 }
