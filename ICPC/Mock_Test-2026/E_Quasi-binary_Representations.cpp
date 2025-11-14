8aa8b92df947746185fc77cff2f991c7fddfcb88
sajjadul404 <sajjadul625@gmail.com>
2025-11-15 00:11:20 +0600

    E_Quasi-binary_Representations

--- ICPC/Mock_Test-2026/E_Quasi-binary_Representations.cpp
+++ ICPC/Mock_Test-2026/E_Quasi-binary_Representations.cpp
@@ -1,24 +1,22 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 const int MOD = 998244353;
 const int BITS = 61;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     int T;
     cin >> T;
-
+    
     while (T--) {
         long long n;
         int k;
         cin >> n >> k;
 
         vector<long long> dp(k + 1, 0);
-        dp[0] = 1;
-
+        dp[0] = 1; 
+        
         for (int pos = 0; pos < BITS; pos++) {
             int need = (n >> pos) & 1;
 
@@ -26,22 +24,24 @@ int main() {
             for (int i = 0; i <= k; i++) {
                 prefix[i + 1] = (prefix[i] + dp[i]) % MOD;
             }
-
+            
             vector<long long> new_dp(k + 1, 0);
-
+         
             for (int new_carry = 0; new_carry <= k; new_carry++) {
+
                 int low = max(0, need + 2 * new_carry - k);
                 int high = min(k, need + 2 * new_carry);
+                
                 if (low <= high) {
                     new_dp[new_carry] = (prefix[high + 1] - prefix[low] + MOD) % MOD;
                 }
             }
-
-            dp = move(new_dp);
+            
+            dp = new_dp;
         }
-
+        
         cout << dp[0] << '\n';
     }
-
+    
     return 0;
 }
