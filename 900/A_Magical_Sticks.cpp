df90eb37dbc89f111d7cc89570801407443f141b
sajjadul404 <sajjadul625@gmail.com>
2025-11-22 00:11:47 +0600

    A Magical Sticks

--- 900/A_Magical_Sticks.cpp
+++ 900/A_Magical_Sticks.cpp
@@ -4,12 +4,10 @@ using namespace std;
 int main() {
     int t;
     cin >> t;
-    while(t--) {
+    while (t--) {
         long long n;
         cin >> n;
-
-        if(n <= 2) cout << 1 << "\n";
-        else cout << n / 2 << "\n";
+        cout << (n + 1) / 2 << "\n";
     }
     return 0;
 }
