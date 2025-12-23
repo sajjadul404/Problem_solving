012a389d7fc2f8eb3f1ec82f3466579e6c885f79
sajjadul404 <sajjadul625@gmail.com>
2025-12-24 00:07:16 +0600

    C wonderful Number

--- Codeforcs Sheet 5/C_Wonderful_Number.cpp
+++ Codeforcs Sheet 5/C_Wonderful_Number.cpp
@@ -1,27 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int odd(int n){
-    if (n%2!=0)
+bool isOdd(int n) {
+    return n % 2 == 1;
+}
 
-void binarypalindrome(int n){
-    string binary = "";
-    while (n>0){
-         binary += (n%2)+'0';
-         n /=2;
-
+bool isBinaryPalindrome(int n) {
+    string bin = "";
+    while (n > 0) {
+        bin += (n % 2) + '0';
+        n /= 2;
     }
-    string reversedBinary = binary;
-    reverse(reversedBinary.begin(), reversedBinary.end());
+
+    string rev = bin;
+    reverse(rev.begin(), rev.end());
+
+    return bin == rev;
 }
 
-int main ()
-{
-   int n ;
-    cin >> n ;
+int main() {
+    int n;
+    cin >> n;
 
+    if (isOdd(n) && isBinaryPalindrome(n))
+        cout << "YES\n";
+    else
+        cout << "NO\n";
 
- return 0;
+    return 0;
 }
-
-
