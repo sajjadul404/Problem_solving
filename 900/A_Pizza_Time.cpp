924bf4290a0a32d53a1e06579da30223900db29f
sajjadul404 <sajjadul625@gmail.com>
2026-02-13 01:27:07 +0600

    A Pizza Time

---
+++ 900/A_Pizza_Time.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+
+#define endl '\n'
+#define prec(x) fixed << setprecision(x)
+
+const ll INF = (1LL << 60);
+const int MOD = 1'000'000'007;
+
+void fastio()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+void solve()
+{
+    ll n;
+    cin>>n;
+
+    cout<<(n-1)/2<<endl;
+}
+
+int main()
+{
+    fastio();
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+}
